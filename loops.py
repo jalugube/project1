2d7b68319ae5fbc94e7f553fbaafd5f2bed9a18a
jalugube <jhansialugubelly44@gmail.com>
2025-01-09 16:17:34 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,143 @@
+# my_list=["dog","cat","rabbit"]
+# print(f"animals:{my_list}")
+
+# my_list=["dior","nars","channel"]
+# print(f"lipsticks:{my_list}")
+
+# my_list=["dog","cat","rabbit"]
+# my_list.append("elephant")
+# print(f"animal:{my_list}")
+
+# my_list=["dog","cat","rabbit","elephant"]
+# my_list.remove("elephant")
+# print(f"animal:{my_list}")
+
+# my_list=["dog","cat","rabbit"]
+# # my_list.insert(1,"tiger")
+# # print(my_list)
+
+# my_list.clear
+# print(f"after clearing:[]")
+
+# my_tuple=("alice","bob","charlie")
+# # print(f"human names:{my_tuple}")
+
+# subtuple=my_tuple[1]
+# print(f"the second name is:{subtuple}")
+
+# my_tuple=("jhan","nikk","nag","chai","abhi")
+# print(f"(the number of names is:{len(my_tuple)}")
+
+# my_list=["dog","cat","rabbit"]
+# my_list.clear+
+# my_list.insert(1,"tiger")
+# print(f"after inserting:{"tiger"}")
+# my_list.remove("cat")
+# print(f"after removing:{my_list}")
+# my_list.append("elephant")
+# print(f"after appending:{my_list}")
+
+# print(f"animals:{my_list}")
+# print(f"after clearing:[]")
+
+# my_tuple=("alice","bob","charlie")
+# print(f"human names:{my_tuple}")
+# sub_tuple=my_tuple[1]
+# print(f"second name is:{sub_tuple}")
+
+# my_tuple=("alice","bob","charlis","jhan","nikk")
+# my_tuple2=("bot1","bot2","bot3")
+# print("my_tuple+my_tuple2")
+# print("bot" in my_tuple)
+# print("charlis" in my_tuple)
+# sub_tuple=my_tuple[0:3]
+# print(f"first three names are:{sub_tuple}")
+
+# print(f"the no.of.names is:{len(my_tuple)}")
+
+# my_set={"lap","smatphone","tablet"}
+# print(f"electronic items:{my_set}")
+# my_set={"laptop","smatphone","tablet"}
+# my_set.add("smart watch")
+# print(f"after adding:{my_set}")
+
+# my_set.remove("tablet")
+# print(f"after removing:{my_set}")
+
+# set_a={"lapi","phn","chy"}
+# set_b={"chy","jhan","nag"}
+# result=set_a|set_b
+# print(result)
+# result_2=set_a&set_b
+# print(result)
+# print(f"intersection result:{result_2}")
+# result_3=set_a-set_b
+# print(f"difference of sets:{result_3}")
+
+# x=10
+# print(type(x))
+# y=1.23456789
+# print(type(y))
+
+# x=5.6
+# y=int(x)
+# print(type(y))
+# z=3+4j
+# y=str(z)
+# print(type(y))
+
+# a=0
+# b=complex(a)
+# print(type(b))
+# b=0
+# c=bool(b)
+# print(type(c))
+
+# x=5.6
+# print(type(x))
+# y=int(x)
+# print(type(y))
+
+# z=1
+# print(bool(z))
+# y=str(z)
+# # print(type(y))
+#  a=3
+# y=complex(a)
+# print(type(y))
+# b=4.5
+# c=complex(b)
+# print(type(c))
+
+# y=8
+# b=int(y)
+# print(type(b))
+# c=float(y)
+# print(type(c))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
